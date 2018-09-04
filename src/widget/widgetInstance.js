c4c685e0802a2ca4d2b14e894c8fbf74dbee189a
mshuu <adrian@bambushu.com>
2018-09-04 11:20:48 +0800

    widget changes

--- src/widget/widgetInstance.js
+++ src/widget/widgetInstance.js
@@ -53,7 +53,7 @@ class WidgetInstance {
     this.iframe.style.cssText = 'width: 100%; height: 100%; overflow: hidden; display: none;' + (this.pollType != 'standard' ? '' : 'border-radius: 3px;');
 
     window.addEventListener("message", this.handleIframeMessage.bind(this), false);
-  
+
     this.pollDiv.appendChild(this.iframe);
   }
 
@@ -66,23 +66,41 @@ class WidgetInstance {
 
   openLogin(){
     this.loginOverlay = document.createElement('div');
-    this.loginOverlay.style.cssText = 'position:absolute;top:0;left:0;width:100%;height:100%;z-index:100;background: rgba(0,0,0,0.8); display: flex; justify-content: center; align-items: center;';
+		this.loginOverlay.id = "loginOverlay";
+    this.loginOverlay.style.cssText = 'position: fixed; top: 0; bottom: 0; left: 0; right: 0;margin: auto;height:550px;width: 670px;z-index:100; display: flex;justify-content: center; align-items: center;border-radius: 7px';
     this.loginOverlay.addEventListener("click", () => this.closeLogin());
+
+	 let closeButton  = document.createElement('a');
+	 closeButton.addEventListener("click", () => this.closeLogin());
+	 let closeImage = document.createElement('img');
+	 closeImage.src = `${SERVER_BASE_URL}/public/close-icon.png`;
+	 closeButton.style.cssText='position:absolute;right: 30px; top: 30px; z-index: 100';
+	 closeButton.appendChild(closeImage);
+	 this.loginOverlay.appendChild(closeButton);
+
+
+	 //<img border="0" alt="W3Schools" src="logo_w3s.gif" width="100" height="100">
+
 
     let loginIframe = document.createElement('iframe');
     loginIframe.src = this.loginUrl;
     loginIframe.frameBorder = 0;
     loginIframe.scrolling = "no";
-    loginIframe.style.cssText = 'width: 670px; height: 550px; overflow: hidden; opacity: 1;';
+    loginIframe.style.cssText = 'width: 670px; height: 550px; overflow: hidden; opacity: 1; \
+		border-radius: 7px;-webkit-box-shadow: 0px 0px 10px 2px rgba(120,120,120,0.1); \
+    -moz-box-shadow: 0px 0px 10px 2px rgba(120,120,120,0.1); box-shadow: 0px 0px 10px 2px rgba(120,120,120,0.1);';
     this.loginOverlay.appendChild(loginIframe);
 
     document.body.appendChild(this.loginOverlay);
   }
 
   closeLogin(){
-    if(!this.loginOverlay) return;
-
-    document.body.removeChild(this.loginOverlay);
+		console.log("This");
+    if(!this.loginOverlay) {
+			console.log("Orthis");
+			return;
+		}
+    document.body.removeChild(document.getElementById("loginOverlay"));
   }
 
   handleIframeMessage(messageEvent){
