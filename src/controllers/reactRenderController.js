6e1a3a367e2abb1102b95d558800e3bf166d0016
Adrian Daluz <adrian@daluz.ca>
2018-10-11 13:10:23 +0800

    'analytics'

--- src/controllers/reactRenderController.js
+++ src/controllers/reactRenderController.js
@@ -500,6 +500,16 @@ const htmlTail = store => `
         window.storeData = "${store}";
       </script>
       <script type="text/javascript" src="/public/bundle.js"></script>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127300472-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-127300472-1');
+</script>
+
     </body>
   </html>
 `;
