46600d61b8e0c616546792f26f35f34fdab17942
Adrian Daluz <adrian@daluz.ca>
2018-12-13 12:30:22 +0800

    's'

--- src/components/viewPoll/components/ResultsGraph.js
+++ src/components/viewPoll/components/ResultsGraph.js
@@ -17,7 +17,6 @@ class ResultGraph extends Component {
 
   get resultsWithPercentages(){
     let results = this.props.poll.results.map(result => {
-      console.log("TOTAL: " + this.totalVotes + " viteCount: " + result.voteCount);
       let votePercentage = this.totalVotes == 0 ? 0 : Math.floor(100 * result.voteCount / this.totalVotes);
       if (votePercentage > 100) {
         votePercentage = 100;
@@ -42,14 +41,11 @@ class ResultGraph extends Component {
 
       results[highestIdx].votePercentage++;
     }
-    console.log("RES: %j", results);
     return results;
   }
 
   graphElement(result, i){
-    console.log("voteCount : " + result.voteCount + " pollVotes: " + this.totalVotes);
     let votePercentage = Math.floor(100 * result.voteCount / (this.totalVotes || 1));
-    console.log("VOTEPERC: " + votePercentage);
     let votedOn = this.props.poll.votedOn && this.props.poll.votedOn[0];
 
 
